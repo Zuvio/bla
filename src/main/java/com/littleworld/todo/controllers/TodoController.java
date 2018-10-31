c478520e4788f00e0db9826ac5b309e3aa358b65
zuvio <Githuberik81>
2018-10-31 13:14:27 +0100

    #alles in git

---
+++ src/main/java/com/littleworld/todo/controllers/TodoController.java
@@ -0,0 +1,47 @@
+package com.littleworld.todo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.littleworld.todo.services.*;
+import com.littleworld.todo.model.*;
+
+@Controller
+public class TodoController {
+
+  @Autowired  
+  private TodoService todoService;
+
+  @RequestMapping(value = "/create", method = RequestMethod.POST)
+  public String create(Todo todo) {
+    todoService.save(todo);
+    return "redirect:/list";
+  }
+  @RequestMapping(value = "/list", method = RequestMethod.GET)
+  public String findAll(Model model) {
+    model.addAttribute("todos", todoService.findAll());
+    return "todoList";
+  }
+  @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+  public String delete(@PathVariable int id) {
+    todoService.delete(id);
+    return "redirect:/list";
+  }
+
+
+  /**nu de fancy manier om het web gedeelte al het werk te laten obv json berichten die hij binnenkrijgt*/
+  @ResponseBody
+  @RequestMapping(value = "/create2", method = RequestMethod.POST)
+  public void create2(@RequestBody Todo todo) {
+    todoService.save(todo);
+  }
+  /**nu de fancy manier om het web gedeelte al het werk te laten obv json berichten die hij binnenkrijgt*/
+  @ResponseBody
+  @RequestMapping(value = "/list2", method = RequestMethod.GET)
+  public Iterable<Todo> findAll2() {
+    return todoService.findAll();
+  }
+}
+
---
+++ src/main/webapp/WEB-INF/views/todoList.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>todo list</title>
+</head>
+<body>
+  <jsp:include page="todoForm.jsp" />
+  <hr>
+  <table>
+    <tr><th>task</th><th>assignee</th><th>DELETE</th></tr>
+ 
+    <c:forEach items="${todos}" var="todo">
+    <tr><td>${todo.task}</td><td>${todo.assignee}</td><td><a href="delete/${todo.id}">delete</a></tr>
+    </c:forEach>  
+
+  </table>
+</body>
+</html>
+
